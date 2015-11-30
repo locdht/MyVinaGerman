14bf5088a4b21faa698059288234800c291f40fe
thuannqd <chieuhoan9@gmail.com>
2015-11-30 20:02:53 +0700

    application

---
+++ VinaGerman.DataSource/IBaseDataDS.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VinaGerman.Entity;
+using VinaGerman.Entity.DatabaseEntity;
+using VinaGerman.Entity.SearchEntity;
+
+namespace VinaGerman.DataSource
+{
+    public interface IBaseDataDS
+    {
+        #region Category
+        List<CategoryEntity> SearchCategories(CategorySearchEntity searchObject);
+        CategoryEntity AddOrUpdateCategory(CategoryEntity entityObject);
+        bool DeleteCategory(CategoryEntity entityObject);
+        #endregion
+
+        #region Business
+        List<BusinessEntity> SearchBusiness(BusinessSearchEntity searchObject);
+        BusinessEntity AddOrUpdateBusiness(BusinessEntity entityObject);
+        bool DeleteBusiness(BusinessEntity entityObject);
+        #endregion
+
+        #region Industry
+        List<IndustryEntity> SearchIndustry(IndustrySearchEntity searchObject);
+        IndustryEntity AddOrUpdateIndustry(IndustryEntity entityObject);
+        bool DeleteIndustry(IndustryEntity entityObject);
+        #endregion
+
+        #region Article
+        List<ArticleEntity> SearchArticle(ArticleSearchEntity searchObject);
+        ArticleEntity AddOrUpdateArticle(ArticleEntity entityObject);
+        bool DeleteArticle(ArticleEntity entityObject);
+        #endregion
+    }
+}
